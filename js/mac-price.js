c3ab2fff8274465f241c98ecb47b2419dd7dd6b8
aaaasif <alexasif4@gmail.com>
2021-08-21 14:15:09 +0600

    total calculate added

--- js/mac-price.js
+++ js/mac-price.js
@@ -1,12 +1,11 @@
 document.getElementById('ram8GB').addEventListener('click', function() {
-    document.getElementById('memory-cost').innerText = 0;
+    document.getElementById('extra-money').innerText = 0;
 })
 document.getElementById('ram16GB').addEventListener('click', function() {
-    document.getElementById('memory-cost').innerText = 180;
+    document.getElementById('extra-money').innerText = 180;
 })
-
 document.getElementById('ssd256GB').addEventListener('click', function() {
-    document.getElementById('storage-cost').innerText = 0;
+    document.getElementById('storage-cost').innerText = 0;  
 })
 document.getElementById('ssd512GB').addEventListener('click', function() {
     document.getElementById('storage-cost').innerText = 100;
@@ -20,3 +19,21 @@ document.getElementById('free-delivery').addEventListener('click', function() {
 document.getElementById('paid-delivery').addEventListener('click', function() {
     document.getElementById('dalivery-cost').innerText = 20;
 })
+
+function totalCalculate() {
+    const bestPrice = document.getElementById('best-price')
+    const getBestPrice = parseInt(bestPrice.innerText)
+
+    const extraMoney = document.getElementById('extra-money')
+    const getExtraMoney = parseInt('extraMoney')
+
+    const storageCost = document.getElementById('storage-cost')
+    const getStorageCost = parseInt('storageCost')
+    
+    const deliveryCost = document.getElementById('dalivery-cost')
+    const getDeliveryCost = parseInt('deliveryCost')
+
+    const totalResult = getBestPrice + getExtraMoney + getStorageCost + getDeliveryCost
+    const totalPrice = document.getElementById('total-price')
+    totalPrice.innerText = totalPrice
+}
