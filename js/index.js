59cb0cb7d773a8532caf12b3a8cba6daf7efb123
aaaasif <alexasif4@gmail.com>
2021-08-21 20:56:20 +0600

    total calculate added

---
+++ js/index.js
@@ -0,0 +1,75 @@
+// 3 functions
+// for extra price and components
+function totalExtraPrice(components, cost) {
+    const extraCost = document.getElementById(components + '-cost');
+    extraCost.innerText = cost;
+}
+// for extra components  
+function forExtraComponents(components) {
+    const componentsPrice = document.getElementById(components + '-cost');
+    const componentsPriceTotal = parseInt(componentsPrice.innerText);
+    return componentsPriceTotal;
+}
+// for update total price
+function finalCostUpdate() {
+    const bestCost = forExtraComponents('best');
+    const memoryCost = forExtraComponents('extra-ram');
+    const storageCost = forExtraComponents('extra-ssd');
+    const deliveryCost = forExtraComponents('extra-delivery')
+    const totalCost = bestCost + memoryCost + storageCost + deliveryCost;
+    document.getElementById('total-price').innerText = totalCost;
+    // for after discount price update
+    const afterDiscountPrice = document.getElementById('after-discount-price');
+    afterDiscountPrice.innerText = totalCost;
+}
+// event listeners
+// for ram
+document.getElementById('8gb-ram').addEventListener('click', function () {
+    totalExtraPrice('extra-ram', 0);
+    finalCostUpdate();
+})
+document.getElementById('16gb-ram').addEventListener('click', function () {
+    totalExtraPrice('extra-ram', 180);
+    finalCostUpdate();
+})
+// for ssd
+document.getElementById('256gb-ssd').addEventListener('click', function () {
+    totalExtraPrice('extra-ssd', 0);
+    finalCostUpdate();
+})
+document.getElementById('512gb-ssd').addEventListener('click', function () {
+    totalExtraPrice('extra-ssd', 100);
+    finalCostUpdate();
+})
+document.getElementById('1tb-ssd').addEventListener('click', function () {
+    totalExtraPrice('extra-ssd', 180);
+    finalCostUpdate();
+})
+// for delivery
+document.getElementById('free-delivery').addEventListener('click', function () {
+    totalExtraPrice('extra-delivery', 0);
+    finalCostUpdate();
+})
+document.getElementById('20$-fast-delivery').addEventListener('click', function () {
+    totalExtraPrice('extra-delivery', 20);
+    finalCostUpdate();
+})
+// promo code apply for 20% discount
+document.getElementById('promocode-apply-btn').addEventListener('click', function () {
+    const totalCost = document.getElementById('total-price').innerText;
+    const afterDiscountPrice = document.getElementById('after-discount-price');
+    const promoCode = document.getElementById('promocode-input');
+    const promoCodeNumber = promoCode.value;
+    if (promoCodeNumber == 'stevekaku') {
+        const discount = totalCost / 5;
+        const priceAfterDiscount = totalCost - discount;
+        afterDiscountPrice.innerText = priceAfterDiscount;
+    }
+    else if (promoCodeNumber == '') {
+        alert('You did not add the promo code.')
+    }
+    else if (promoCodeNumber != 'stevekaku') {
+        alert('You have inserted the wrong promo code.')
+    }
+    promoCode.value = '';
+})
